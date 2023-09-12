8fbfa834c36a363ffece783a27593198384f1bf4
Max Kachinkin <makzimi@gmail.com>
2023-09-12 19:51:18 +0300

    Prepare helper method createCharacter

--- app/src/test/java/ru/otus/tomvi/presentation/start/CharactersViewModelTest.kt
+++ app/src/test/java/ru/otus/tomvi/presentation/start/CharactersViewModelTest.kt
@@ -6,6 +6,7 @@ import org.junit.Rule
 import org.junit.runner.RunWith
 import org.mockito.junit.MockitoJUnitRunner
 import ru.otus.tomvi.MainCoroutineRule
+import ru.otus.tomvi.data.RaMCharacter
 
 @OptIn(ExperimentalCoroutinesApi::class)
 @RunWith(MockitoJUnitRunner.Silent::class)
@@ -14,4 +15,15 @@ class CharactersViewModelTest {
     @get:Rule
     var mainCoroutineRule = MainCoroutineRule()
 
+    private fun createCharacter(
+        id: Long = 0L,
+        name: String = "",
+        image: String = "",
+    ): RaMCharacter {
+        return RaMCharacter(
+            id = id,
+            name = name,
+            image = image,
+        )
+    }
 }
